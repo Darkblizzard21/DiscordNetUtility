41841d7c3a84e625112a765e232d101b368d227c
Pirmin Pfeifer <pirmin.pfeifer@gmx.de>
2022-01-03 06:11:12 +0100

    📝 updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # DiscordNetUtility
+
+## About
+
+DisordNetutility is a small Libary that eases the development of discord bots in [C# (Discord.Net)](https://github.com/discord-net/Discord.Net).
--- src/SlashCommandUtil.cs
+++ src/SlashCommandUtil.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Discord;
-using Discord.Net;
 using Discord.WebSocket;
-using Newtonsoft.Json;
 
 namespace DNU
 {
