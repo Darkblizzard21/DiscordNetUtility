ebab6dc2b5efc5fa6db71210ecc2d91f273d2588
Pirmin Pfeifer <pirmin.pfeifer@gmx.de>
2022-01-03 02:09:25 +0100

    ✨ New User Interaction Util and GuildChannelExtension

---
+++ src/GuildChannelExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Discord;
+
+namespace DNU
+{
+    public static class GuildChannelExtensions
+    {
+        public static string GetMentionOrName(this IGuildChannel channel)
+        {
+            if (channel is ITextChannel textChannel)
+            {
+                return textChannel.Mention;
+            }
+            return channel.Name;
+        }
+    }
+}
---
+++ src/UserInteraction.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Discord.WebSocket;
+
+namespace DNU
+{
+    public static class UserInteraction
+    {
+        public static bool IsAdmin(SocketGuildUser user)
+        {
+            return user.GuildPermissions.Administrator;
+        }
+
+        public static async Task<bool> CheckAdmin(this SocketSlashCommand command, SocketGuildUser user)
+        {
+            var res = IsAdmin(user);
+            if(!res) await command.RespondAsync($"Command can only be invoked by admins", ephemeral: true);
+            return res;
+        }
+    }
+}
